28f807820a2c7651b1f9559134f34c4a6aa06e2f
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-31 20:11:51 +0300

    add LoginXpath

---
+++ src/test/java/by/onliner/LoginXpath.java
@@ -0,0 +1,8 @@
+package by.onliner;
+
+public class LoginXpath {
+
+    public static final String BUTTON_ENTER_XPATH = "//button[@type='submit']";
+    public static final String INPUT_EMAIL_XPATH ="//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div";
+    public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/input";
+}
