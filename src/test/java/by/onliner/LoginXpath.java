5a8ab8650cd0b89ad68f8d45de016e24c697af27
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-10-31 20:36:10 +0300

    refactor LoginXpath add ERROR_MESSAGE_ENTER_EMAIL_XPATH, ERROR_MESSAGE_ENTER_PASSWORD_XPATH, INVALID_LOGIN_OR_PASSWORD_XPATH

--- src/test/java/by/onliner/LoginXpath.java
+++ src/test/java/by/onliner/LoginXpath.java
@@ -5,4 +5,7 @@ public class LoginXpath {
     public static final String BUTTON_ENTER_XPATH = "//button[@type='submit']";
     public static final String INPUT_EMAIL_XPATH ="//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div";
     public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div/div/input";
+    public static final String ERROR_MESSAGE_ENTER_EMAIL_XPATH = "/html/body/div[4]/div/div/div/div/div/div/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static final String ERROR_MESSAGE_ENTER_PASSWORD_XPATH = "/html/body/div[4]/div/div/div/div/div/div/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+    public static final String INVALID_LOGIN_OR_PASSWORD_XPATH = "/html/body/div[4]/div/div/div/div/div/div/div/div[2]/div/form/div[3]/div";
 }
